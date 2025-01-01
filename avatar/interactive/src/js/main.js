aef79f786b081a79a093ed076a9acff5e9981f29
ash1197 <ashishkumar.shyam@infosys.com>
2025-01-01 17:49:23 +0530

    Update main.js

--- avatar/interactive/src/js/main.js
+++ avatar/interactive/src/js/main.js
@@ -20,7 +20,7 @@ const IceServerUrl = "turn:relay.communication.microsoft.com:3478" // Fill your 
 let IceServerUsername
 let IceServerCredential
 
-const TalkingAvatarCharacter = "lisa"
+const TalkingAvatarCharacter = "ashish"
 const TalkingAvatarStyle = "graceful-standing"
 
 supported_languages = ["en-US", "de-DE", "zh-CN", "ar-AE"] // The language detection engine supports a maximum of 4 languages
