bae48085afc3ac4cbd1ea876e04d38a1cb253e33
ash1197 <ashishkumar.shyam@infosys.com>
2025-01-01 16:47:25 +0530

    Update main.js

--- avatar/interactive/src/js/main.js
+++ avatar/interactive/src/js/main.js
@@ -21,7 +21,7 @@ let IceServerUsername
 let IceServerCredential
 
 const TalkingAvatarCharacter = "lisa"
-const TalkingAvatarStyle = "graceful-sitting"
+const TalkingAvatarStyle = "technical-standing"
 
 supported_languages = ["en-US", "de-DE", "zh-CN", "ar-AE"] // The language detection engine supports a maximum of 4 languages
 
